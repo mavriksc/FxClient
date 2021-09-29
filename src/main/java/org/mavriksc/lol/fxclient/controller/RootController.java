563d43b29e4774edb642ce6b221cf22e002a73b9
Sean Carlisle <scarlisle@hylamobile.com>
2021-09-29 08:10:26 -0500

    Controller means web request handler in this case use component instead

--- src/main/java/org/mavriksc/lol/fxclient/controller/RootController.java
+++ src/main/java/org/mavriksc/lol/fxclient/controller/RootController.java
@@ -3,25 +3,17 @@ package org.mavriksc.lol.fxclient.controller;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.util.Duration;
-import org.mavriksc.lol.fxclient.dto.MatchDTO;
-import org.mavriksc.lol.fxclient.dto.MatchShortDTO;
-import org.mavriksc.lol.fxclient.dto.Participant;
-import org.mavriksc.lol.fxclient.dto.ParticipantIdentity;
-import org.mavriksc.lol.fxclient.dto.SummonerDTO;
+import org.mavriksc.lol.fxclient.dto.*;
 import org.mavriksc.lol.fxclient.service.MatchService;
 import org.mavriksc.lol.fxclient.service.SummonerService;
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Component;
 
 import java.util.Date;
 import java.util.List;
 
-@Controller
+@Component
 public class RootController {
     @FXML
     private TextField summonerNameTextField;
@@ -50,7 +42,7 @@ public class RootController {
         String name = summonerNameTextField.getText();
         SummonerDTO summoner = summonerService.getSummonerByName(name);
         List<MatchShortDTO> matches = matchService.getMatchListForUser(summoner.getAccountId()).getMatches();
-        summonerLbl.setText(name+" "+ new Date(matches.get(0).getTimestamp()).toString());
+        summonerLbl.setText(name + " " + new Date(matches.get(0).getTimestamp()).toString());
         Timeline timeline = new Timeline(new KeyFrame(Duration.millis(2100), event -> {
             if (liveGameCkbx.isSelected()) {
                 MatchDTO match = matchService.getMatchByID(matches.get(0).getGameId());
